97166191b0b04e995c4db94b17a9f02af15ada41
Anton Dehiarenko <anton.dehtiarenko@chdp-tech.net>
2023-05-19 09:38:23 +0200

    mock aiohttp async context manager

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,50 +1,48 @@
 from xecd_rates_client import XecdClient
 import unittest
-from unittest import mock
+import aiohttp
+from unittest.mock import MagicMock
 import json
-import copy
+
 with open('tests/data/testdata.json') as json_data:
     data = json.load(json_data)
 
 
-class MockResponse:
-    def __init__(self, json_data, status_code):
-        self.json_data = copy.deepcopy(json_data)
-        self.status_code = status_code
+class XecdClientUnitTest(unittest.IsolatedAsyncioTestCase):
 
-    def json(self):
-        return self.json_data
+    def mock_session(self, data, value):
+        mock = aiohttp.ClientSession
+        mock.get = MagicMock()
+        mock.get.return_value.__aenter__.return_value.status = value
+        mock.get.return_value.__aenter__.return_value.json.return_value = data
 
-
-class XecdClientUnitTest(unittest.TestCase):
-
-    def setUp(self):
+    async def asyncSetUp(self):
         self.xecd = XecdClient('accountId', 'apiKey')
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeAccountInfo"], 200))
-    def testAccountInfo(self, mock_get):
-        self.assertEqual(data["fakeAccountInfo"], self.xecd.account_info())
+    async def testAccountInfo(self):
+        self.mock_session(data=data["fakeAccountInfo"], value=200)
+        self.assertEqual(data["fakeAccountInfo"], await self.xecd.account_info())
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeCurrencies"], 200))
-    def testCurrencies(self, mock_get):
-        self.assertEqual(data["fakeCurrencies"], self.xecd.currencies())
+    async def testCurrencies(self):
+        self.mock_session(data=data["fakeCurrencies"], value=200)
+        self.assertEqual(data["fakeCurrencies"], await self.xecd.currencies())
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeConvertFrom"], 200))
-    def testConvertFrom(self, mock_get):
-        self.assertEqual(data["fakeConvertFrom"], self.xecd.convert_from("EUR", "CAD", 55))
+    async def testConvertFrom(self):
+        self.mock_session(data=data["fakeConvertFrom"], value=200)
+        self.assertEqual(data["fakeConvertFrom"], await self.xecd.convert_from("EUR", "CAD", 55))
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeConvertTo"], 200))
-    def testConvertTo(self, mock_get):
-        self.assertEqual(data["fakeConvertTo"], self.xecd.convert_to("RUB", "CAD", 55))
+    async def testConvertTo(self):
+        self.mock_session(data=data["fakeConvertTo"], value=200)
+        self.assertEqual(data["fakeConvertTo"], await self.xecd.convert_to("RUB", "CAD", 55))
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeHistoricRate"], 200))
-    def testHistoricRate(self, mock_get):
-        self.assertEqual(data["fakeHistoricRate"], self.xecd.historic_rate("2016-12-25", "12:34", "EUR", "CAD", 55))
+    async def testHistoricRate(self):
+        self.mock_session(data=data["fakeHistoricRate"], value=200)
+        self.assertEqual(data["fakeHistoricRate"], await self.xecd.historic_rate("2016-12-25", "12:34", "EUR", "CAD", 55))
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeHistoricRatePeriod"], 200))
-    def testHistoricRatePeriod(self, mock_get):
-        self.assertEqual(data["fakeHistoricRatePeriod"], self.xecd.historic_rate_period(55, "EUR", "RUB", "2016-02-28T12:00", "2016-03-03T12:00"))
+    async def testHistoricRatePeriod(self):
+        self.mock_session(data=data["fakeHistoricRatePeriod"], value=200)
+        self.assertEqual(data["fakeHistoricRatePeriod"], await self.xecd.historic_rate_period(55, "EUR", "RUB", "2016-02-28T12:00", "2016-03-03T12:00"))
 
-    @mock.patch('requests.get', return_value=MockResponse(data["fakeMonthlyAverage"], 200))
-    def testMonthlyAverage(self, mock_get):
-        self.assertEqual(data["fakeMonthlyAverage"], self.xecd.monthly_average(55, "CAD", "EUR", 2017, 5))
+    async def testMonthlyAverage(self):
+        self.mock_session(data=data["fakeMonthlyAverage"], value=200)
+        self.assertEqual(data["fakeMonthlyAverage"], await self.xecd.monthly_average(55, "CAD", "EUR", 2017, 5))
